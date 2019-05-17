c9d503abc2ec3547b791adf7ffd653a640e2ccf0
KB Choi <kwangbom@gmail.com>
2019-05-17 17:34:03 -0400

    [kbchoi] Working on 'run_em'

--- tenxt/__init__.py
+++ tenxt/__init__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
-"""Top-level package for TENXT."""
+"""Top-level package for PoolClass"""
 import os
 
 __author__ = 'Kwangbom \"KB\" Choi, Ph.D.'
 __email__ = 'kb.choi@jax.org'
 __version__ = '0.1.0'
 __logo__ = """
-_____  ____  _      _    _____ 
- | |  | |_  | |\ | \ \_/  | |  
- |_|  |_|__ |_| \| /_/ \  |_|  
-                        v""" + __version__
+ ___   ___   ___   _     __    _      __    __   __  
+| |_) / / \ / / \ | |   / /`  | |    / /\  ( (` ( (` 
+|_|   \_\_/ \_\_/ |_|__ \_\_, |_|__ /_/--\ _)_) _)_) 
+                                            v""" + __version__
 __all__ = ['.']
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
