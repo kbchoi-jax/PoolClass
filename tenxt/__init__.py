15da2e08004097ea86c5158ef8f445fff339c61e
KB Choi <kb.choi@jax.org>
2019-05-18 01:47:45 -0400

    [kbchoi] Testing 'run_em'

--- tenxt/__init__.py
+++ tenxt/__init__.py
@@ -10,11 +10,10 @@ __logo__ = """
  ___   ___   ___   _     __    _      __    __   __  
 | |_) / / \ / / \ | |   / /`  | |    / /\  ( (` ( (` 
 |_|   \_\_/ \_\_/ |_|__ \_\_, |_|__ /_/--\ _)_) _)_) 
-                                            v""" + __version__
+                                              v""" + __version__
 __all__ = ['.']
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
-
 
 def get_data(path):
     return os.path.join(_ROOT, 'external', path)
