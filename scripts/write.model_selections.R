ffe156437665a56535a3c533b59e68eac1d3711b
KB Choi <kb.choi@jax.org>
2019-06-13 13:49:46 -0400

    [kbchoi] Fixed write.model_selection

--- scripts/write.model_selections.R
+++ scripts/write.model_selections.R
@@ -10,28 +10,28 @@ attr_name <- args[3]
 
 library(loomR)
 
-ds <- connect(loomfile, mode='r+')
 results <- readRDS(loo_results)
-results <- results[!is.na(rowSums(results)), ]
+#results <- results[!is.na(rowSums(results)), ]
+gsurv <- names(results)
 
-best1 <- results[,1]<0 & results[,3]<0
-best2 <- results[,1]>0 & results[,5]<0
-best3 <- results[,3]>0 & results[,5]>0
-cat(sprintf('%5d Poisson genes\n', sum(best1)))
-cat(sprintf('%5d Negative Binomial genes\n', sum(best2)))
-cat(sprintf('%5d Zero-Inflated Neg. Binomial genes\n', sum(best3)))
-cat(sprintf('%5d Total\n', sum(best1, best2, best3)))
+bestmodel <- c()
+for (g in gsurv) {
+  res <- results[[g]]
+  bestmodel <- c(bestmodel, as.integer(gsub('model', '', rownames(res)[1])))
+}
+cat(sprintf('%5d Poisson genes\n', sum(bestmodel==1)))
+cat(sprintf('%5d Negative Binomial genes\n', sum(bestmodel==2)))
+cat(sprintf('%5d Zero-Inflated Poisson genes\n', sum(bestmodel==3)))
+cat(sprintf('%5d Zero-Inflated Neg. Binomial genes\n', sum(bestmodel==4)))
+cat(sprintf('%5d Total\n', length(results)))
 
+ds <- connect(loomfile, mode='r+')
 gname <- ds$row.attrs$GeneID[]
-best1idx <- match(rownames(results[best1,]), gname)
-best2idx <- match(rownames(results[best2,]), gname)
-best3idx <- match(rownames(results[best3,]), gname)
-bestmodel <- rep('', length(gname))
-bestmodel[best1idx] <- 'P'
-bestmodel[best2idx] <- 'NB'
-bestmodel[best3idx] <- 'ZINB'
+best_idx <- match(rownames(results), gname)
+bestmodel_fullsize <- rep(0, length(gname))
+bestmodel_fullsize[best_idx] <- bestmodel
 ra <- vector(mode="list", length=1)
 names(ra) <- attr_name
-ra[[attr_name]] <- bestmodel
+ra[[attr_name]] <- bestmodel_fullsize
 ds$add.row.attribute(ra, overwrite=TRUE)
 ds$close_all()
