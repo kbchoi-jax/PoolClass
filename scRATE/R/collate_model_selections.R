f26f6e84b2fe9a28c7887484b85007dbabe951d2
KB Choi <kb.choi@jax.org>
2019-07-17 09:59:37 -0400

    [kbchoi] Updated utility functions

--- scRATE/R/collate_model_selections.R
+++ scRATE/R/collate_model_selections.R
@@ -1,10 +1,10 @@
 #' Bayesian linear regression with Stan
 #'
 #' @export
-#' @param loomfile Numeric vector of UMI counts.
-#' @param loo_results Numeric vector of cell sizes (total UMI counts per cell).
-#' @param attr_name Name of the .
-#' @return A list of `stanfit` returned by four models: Poisson, Negative-Binomial, Zero-Inflated Poisson, & Zero-Inflated Negative-Binomial
+#' @param loomfile A expression quantity file (loom format).
+#' @param loo_results Leave-one-out ELPD result file.
+#' @param attr_name Name of the row attribute in loomfile for storing best model calls.
+#' @return best_model_calls Best model calls are also stored in the input loomfile.
 #'
 collate_model_selections <- function(loomfile, loo_results, attr_name) {
   results <- readRDS(loo_results)
@@ -13,7 +13,33 @@ collate_model_selections <- function(loomfile, loo_results, attr_name) {
   bestmodel <- c()
   for (g in gsurv) {
     res <- results[[g]]
-    bestmodel <- c(bestmodel, as.integer(gsub('model', '', rownames(res)[1])))
+    if (rownames(res)[1] == 'model1') {
+      bestmodel <- c(bestmodel, 1)
+    } else if (rownames(res)[1] == 'model2') {
+      if (abs(res['model1',][['elpd_diff']]) < 2 * res['model1',][['se_diff']]) {
+        bestmodel <- c(bestmodel, 1)
+      } else {
+        bestmodel <- c(bestmodel, 2)
+      }
+    } else if (rownames(res)[1] == 'model3') {
+      if (abs(res['model1',][['elpd_diff']]) < 2 * res['model1',][['se_diff']]) {
+        bestmodel <- c(bestmodel, 1)
+      } else if (abs(res['model2',][['elpd_diff']]) < 2 * res['model2',][['se_diff']]) {
+        bestmodel <- c(bestmodel, 2)
+      } else {
+        bestmodel <- c(bestmodel, 3)
+      }
+    } else if (rownames(res)[1] == 'model4') {
+      if (abs(res['model1',][['elpd_diff']]) < 2 * res['model1',][['se_diff']]) {
+        bestmodel <- c(bestmodel, 1)
+      } else if (abs(res['model2',][['elpd_diff']]) < 2 * res['model2',][['se_diff']]) {
+        bestmodel <- c(bestmodel, 2)
+      } else if (abs(res['model3',][['elpd_diff']]) < 2 * res['model3',][['se_diff']]) {
+        bestmodel <- c(bestmodel, 3)
+      } else {
+        bestmodel <- c(bestmodel, 4)
+      }
+    }
   }
   cat(sprintf('%5d Poisson genes\n', sum(bestmodel==1)))
   cat(sprintf('%5d Negative Binomial genes\n', sum(bestmodel==2)))
@@ -23,7 +49,7 @@ collate_model_selections <- function(loomfile, loo_results, attr_name) {
 
   ds <- connect(loomfile, mode='r+')
   gname <- ds$row.attrs$GeneID[]
-  best_idx <- match(rownames(results), gname)
+  best_idx <- match(gsurv, gname)
   bestmodel_fullsize <- rep(0, length(gname))
   bestmodel_fullsize[best_idx] <- bestmodel
   ra <- vector(mode="list", length=1)
@@ -31,4 +57,5 @@ collate_model_selections <- function(loomfile, loo_results, attr_name) {
   ra[[attr_name]] <- bestmodel_fullsize
   ds$add.row.attribute(ra, overwrite=TRUE)
   ds$close_all()
+  return(bestmodel_fullsize)
 }
--- scRATE/R/run_model_comparison.R
+++ scRATE/R/run_model_comparison.R
@@ -1,31 +1,33 @@
-#' Bayesian linear regression with Stan
+#' Bayesian model selection using loo
 #'
 #' @export
-#' @param cntfile Numeric vector of UMI counts.
-#' @param outfile Numeric vector of cell sizes (total UMI counts per cell).
+#' @param cntfile Expression quantity file (RData format: Use 'save' and 'load').
+#' @param outfile Output file name to store ELPD_loo results (RDS format).
 #' @param nCores Number of cores.
 #' @param seed Seed number.
-#' @param ... Arguments passed to `rstan::sampling` (e.g. iter, chains).
-#' @return A list of `stanfit` returned by four models: Poisson, Negative-Binomial, Zero-Inflated Poisson, & Zero-Inflated Negative-Binomial
+#' @return A list of ELPD_loo results returned by loo::compare
 #'
-run_model_comparison <- function(cntfile, outfile, nCores, seed) {
-  nCores <- min(4, parallel::detectCores())
+run_model_comparison <- function(cntfile, nCores=NULL, seed=NULL, outfile=NULL) {
+  if(is.null(nCores)) {
+    nCores <- parallel::detectCores()
+  }
+  if(is.null(seed)) {
+    seed <- 1004
+  }
+
   load(cntfile)  # This will load 'cntmat', 'gsurv', and 'csize'
   gname <- rownames(cntmat)
   num_genes <- length(gsurv)
   exposure <- log(csize)
 
-  #zip_model  <- readRDS('zip.rds')
-  #zinb_model <- readRDS('zinb.rds')
   results <- list()
-  processed <- c()
-
   for (gg in c(1:num_genes)) {
     if(gsurv[gg]) {
       y <- round(unlist(cntmat[gg,]))
       cat(sprintf("\nTesting %s\n", gname[gg]))
       tryCatch({
-        res <- compare_count_models(y, exposure, nCores, seed)
+        fit <- fit_count_models(y, exposure, nCores, seed)
+        res <- compare_count_models(fit)
         results[[gname[gg]]] <- res
       }, error = function(err) {
         cat(sprintf("Error while fitting %s\n", gname[gg]))
@@ -33,5 +35,8 @@ run_model_comparison <- function(cntfile, outfile, nCores, seed) {
     }
   }
 
-  saveRDS(results, file = outfile)
+  if(!is.null(outfile)) {
+    saveRDS(results, file = outfile)
+  }
+  return(results)
 }
