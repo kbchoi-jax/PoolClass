6df43a9ba1a7433abe89276fcd0e24decfa85b48
KB Choi <kb.choi@jax.org>
2019-07-24 21:28:41 -0400

    [kbchoi] Debugged submit_jobs.R

--- scRATE/R/submit_jobs.R
+++ scRATE/R/submit_jobs.R
@@ -58,27 +58,26 @@ submit_jobs <- function(loomfile, num_chunks, outdir, dryrun, scriptfile, rfile,
   idx_gsurv <- which(selected > 0)
   idx_gsurv <- idx_gsurv[idx_gsurv >= gidx1 & idx_gsurv <= gidx2]
   num_gsurv <- length(idx_gsurv)
-  chunk_sz <- ceiling(num_gsurv / num_chunks)
-  gsets <- split(idx_gsurv, ceiling(seq_along(idx_gsurv) / chunk_sz))
-
   cat(sprintf('[submit_jobs] %d genes (between Gene %d and %d) will be processed.\n', num_gsurv, gidx1, gidx2))
 
-  g_ends <- c()
-  for (k in 1:length(gsets)) {
-    g_ends <- c(g_ends, tail(gsets[[k]], 1))
-  }
+  # chunk_sz <- ceiling(num_gsurv / num_chunks)
+  # gsets <- split(idx_gsurv, ceiling(seq_along(idx_gsurv) / chunk_sz))
+  # g_ends <- c()
+  # for (k in 1:length(gsets)) {
+  #   g_ends <- c(g_ends, tail(gsets[[k]], 1))
+  # }
+  chunk_sz <- num_gsurv / num_chunks
+  chunk_end_idx <- round(chunk_sz * 1:num_chunks)
+  g_ends <- idx_gsurv[chunk_end_idx]
   g_starts <- g_ends + 1
   g_starts <- c(gidx1, g_starts)
   g_starts <- g_starts[-length(g_starts)]
   g_ends[length(g_ends)] <- gidx2
-
+  
   dmat <- as.data.frame(t(dmat))
   rownames(dmat) <- gname
   colnames(dmat) <- cname
   csize <- as.vector(colSums(dmat))
-
-  #scriptfile <- '/home/kbchoi/src/utils/submit_model_comparison_on_cluster.sh'
-  #rfile <- 'run_model_comparison.R'
 
   if (is.null(chunk_start)) {
     cidx1 <- 1
@@ -96,7 +95,6 @@ submit_jobs <- function(loomfile, num_chunks, outdir, dryrun, scriptfile, rfile,
     }
     cidx2 <- chunk_end
   }
-
   cat(sprintf('[submit_jobs] Chunk %d to %d (out of %d) will be processed.\n', cidx1, cidx2, num_chunks))
 
   for (k in cidx1:cidx2) {
