8d57660d2f48359460111c5fd17232d0401ee4da
KB Choi <kb.choi@jax.org>
2019-09-19 19:15:11 -0400

    [kbchoi] A quick fix

--- scRATE/R/get_bestmodel_fit.R
+++ scRATE/R/get_bestmodel_fit.R
@@ -29,7 +29,7 @@ get_bestmodel_fit <- function(cntfile, nCores=NULL, seed=NULL, outfile=NULL) {
       tryCatch({
         #results[[gname[gg]]] <- fit_count_models(y, exposure, nCores, seed, model2fit=model2fit[gg], brms4zi=TRUE)
         model_fit <- fit_count_models(y, exposure, nCores, seed, model2fit=4, brms4zi=TRUE)
-        results[[gname[gg]]] <- colMeans(as.matrix(model_fit, pars = c("b_Intercept", "b_zi_Intercept", "shape")))
+        results[[gname[gg]]] <- colMeans(as.matrix(model_fit[['ZINB']], pars = c("b_Intercept", "b_zi_Intercept", "shape")))
       }, error = function(err) {
         cat(sprintf("Error while fitting %s\n", gname[gg]))
       })
