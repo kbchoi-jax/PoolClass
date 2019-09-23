0776cb5ca77ef29f1e90ef27038a6b3ee73836c8
KB Choi <kb.choi@jax.org>
2019-09-23 14:18:23 -0400

    [kbchoi] A quick fix in is_bestfit.R

--- scRATE/R/is_bestfit.R
+++ scRATE/R/is_bestfit.R
@@ -15,9 +15,9 @@ is_bestfit <- function(loo_results, model2check, margin=2) {
 
   if (model2check == 1) {
     if (rownames(loo_results)[1] == 'model1') {
-      if (abs(loo_results['model2',][['elpd_diff']]) < margin * loo_results['model2',][['se_diff']] &&
-          abs(loo_results['model3',][['elpd_diff']]) < margin * loo_results['model3',][['se_diff']] &&
-          abs(loo_results['model4',][['elpd_diff']]) < margin * loo_results['model4',][['se_diff']]) 
+      if (abs(loo_results['model2',][['elpd_diff']]) > margin * loo_results['model2',][['se_diff']] &&
+          abs(loo_results['model3',][['elpd_diff']]) > margin * loo_results['model3',][['se_diff']] &&
+          abs(loo_results['model4',][['elpd_diff']]) > margin * loo_results['model4',][['se_diff']]) 
       {
         return(TRUE)
       } else {
@@ -28,9 +28,9 @@ is_bestfit <- function(loo_results, model2check, margin=2) {
     }
   } else if (model2check == 2) {
     if (rownames(loo_results)[1] == 'model2') {
-      if (abs(loo_results['model3',][['elpd_diff']]) < margin * loo_results['model3',][['se_diff']] &&
-          abs(loo_results['model4',][['elpd_diff']]) < margin * loo_results['model4',][['se_diff']] &&
-          abs(loo_results['model1',][['elpd_diff']]) < margin * loo_results['model1',][['se_diff']]) 
+      if (abs(loo_results['model3',][['elpd_diff']]) > margin * loo_results['model3',][['se_diff']] &&
+          abs(loo_results['model4',][['elpd_diff']]) > margin * loo_results['model4',][['se_diff']] &&
+          abs(loo_results['model1',][['elpd_diff']]) > margin * loo_results['model1',][['se_diff']]) 
       {
         return(TRUE)
       } else {
@@ -41,9 +41,9 @@ is_bestfit <- function(loo_results, model2check, margin=2) {
     }
   } else if (model2check == 3) {
     if (rownames(loo_results)[1] == 'model3') {
-      if (abs(loo_results['model4',][['elpd_diff']]) < margin * loo_results['model4',][['se_diff']] &&
-          abs(loo_results['model1',][['elpd_diff']]) < margin * loo_results['model1',][['se_diff']] &&
-          abs(loo_results['model2',][['elpd_diff']]) < margin * loo_results['model2',][['se_diff']]) 
+      if (abs(loo_results['model4',][['elpd_diff']]) > margin * loo_results['model4',][['se_diff']] &&
+          abs(loo_results['model1',][['elpd_diff']]) > margin * loo_results['model1',][['se_diff']] &&
+          abs(loo_results['model2',][['elpd_diff']]) > margin * loo_results['model2',][['se_diff']]) 
       {
         return(TRUE)
       } else {
@@ -54,9 +54,9 @@ is_bestfit <- function(loo_results, model2check, margin=2) {
     }
   } else if (model2check == 4) {
     if (rownames(loo_results)[1] == 'model4') {
-      if (abs(loo_results['model1',][['elpd_diff']]) < margin * loo_results['model1',][['se_diff']] &&
-          abs(loo_results['model2',][['elpd_diff']]) < margin * loo_results['model2',][['se_diff']] &&
-          abs(loo_results['model3',][['elpd_diff']]) < margin * loo_results['model3',][['se_diff']]) 
+      if (abs(loo_results['model1',][['elpd_diff']]) > margin * loo_results['model1',][['se_diff']] &&
+          abs(loo_results['model2',][['elpd_diff']]) > margin * loo_results['model2',][['se_diff']] &&
+          abs(loo_results['model3',][['elpd_diff']]) > margin * loo_results['model3',][['se_diff']]) 
       {
         return(TRUE)
       } else {
